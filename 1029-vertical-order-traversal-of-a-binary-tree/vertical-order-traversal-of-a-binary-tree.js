d43aa68765e578a8b287a5cbbee9a4cad64e5cd5
Dhruv Gupta <162358068+dhruvG110@users.noreply.github.com>
2026-08-03 04:31:38 +0530

    Time: 5 ms (16.56%) | Memory: 55.9 MB (73.51%) - LeetSync

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.js
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var verticalTraversal = function (root) {
+    if (root == null) return [];
+    let queue = [];
+    let ans = [];
+    const map = new Map();
+    queue.push([root, 0, 0]);
+    while (queue.length != 0) {
+        let level = []
+        let size = queue.length;
+        for (let i = 0; i < size; i++) {
+            let [node, row, col] = queue.shift();
+            level.push([node.val, row, col]);
+            if (!map.has(col)) {
+                map.set(col, []);
+            }
+            map.get(col).push([row, node.val]);
+            if (node.left != null) queue.push([node.left, row + 1, col - 1]);
+            if (node.right != null) queue.push([node.right, row + 1, col + 1]);
+        }
+        ans.push(level);
+    }
+    let vT = [];
+    let maxIndex = -Infinity;
+    let minIndex = Infinity;
+    for (let [key, val] of map) {
+        maxIndex = Math.max(maxIndex, key);
+    }
+    for (let [key, val] of map) {
+        minIndex = Math.min(minIndex, key);
+    }
+    for (let [col, arr] of map) {
+        arr.sort((a, b) => {
+            if (a[0] === b[0]) return a[1] - b[1];
+            return a[0] - b[0];
+        });
+    }
+    for (let i = minIndex; i <= maxIndex; i++) {
+
+        if (!map.has(i)) continue;
+
+        let temp = [];
+
+        for (let [row, val] of map.get(i)) {
+            temp.push(val);
+        }
+
+        vT.push(temp);
+    }
+    return vT;
+};
