f04623e5bf72a72b76a6287c5eda6ce473587f41
Dhruv Gupta <162358068+dhruvG110@users.noreply.github.com>
2026-08-04 02:06:56 +0530

    Time: 0 ms (100.00%) | Memory: 57.2 MB (63.10%) - LeetSync

---
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function(root) {
+   let prev = null;
+   const dfs =(root)=>{
+    if(root == null)return;
+    dfs(root.right);
+    dfs(root.left);
+    root.right = prev;
+    root.left = null;
+    prev = root;
+   }
+   return dfs(root);
+};
