be78177959f7aefceb3961158646874a28c020c2
Dhruv Gupta <162358068+dhruvG110@users.noreply.github.com>
2026-09-07 14:53:38 +0530

    Time: 44 ms (100.00%) | Memory: 61.5 MB (100.00%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.js
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.js
@@ -1,102 +1,40 @@
-/**
- * @param {number[][]} heights
- * @return {number}
- */
-var minimumEffortPath = function (heights) {
-    let rows = heights.length;
-    let columns = heights[0].length;
-    let effort = Array.from({ length: rows }, () => Array(columns).fill(Infinity));
-    let heap = new MyMinHeap();
+var minimumEffortPath = function(heights) {
+    let n = heights.length;
+    let m = heights[0].length;
 
-    effort[0][0] = 0;
-    heap.insert([0, 0, 0])
-    let edges = [
-        [-1, 0],
-        [0, -1],
-        [1, 0],
-        [0, 1]
-    ];
-    while (heap.size() > 0) {
-        let [row, column, currentEffort] = heap.removeMin();
-        if (currentEffort > effort[row][column]) {
-            continue;
-        }
-        /* if (row == rows - 1 && column == columns - 1) {
-            return currentEffort;
-        } */
-        for (let [dr, dc] of edges) {
-            const newRow = row + dr;
-            const newCol = column + dc;
-            if (
-                newRow < 0 ||
-                newRow >= rows ||
-                newCol < 0 ||
-                newCol >= columns
-            ) {
-                continue;
-            }
+    let pq = [[0, 0, 0]]; // [effort, row, col]
+    let dist = Array.from({ length: n }, () => Array(m).fill(1e9));
+    dist[0][0] = 0;
 
-            let newEffort = Math.max(currentEffort, Math.abs(heights[newRow][newCol] - heights[row][column]));
-            if (newEffort < effort[newRow][newCol]) {
-                effort[newRow][newCol] = newEffort;
-                heap.insert([newRow, newCol, newEffort]);
+    let dir = [[0, 1], [0, -1], [1, 0], [-1, 0]];
+
+    while(pq.length > 0){
+        let [diff, r, c] = pq.pop();
+
+        if(r === n-1 && c === m-1) return diff;
+
+        for(let k = 0; k < 4; k++){
+            let nr = r + dir[k][0];
+            let nc = c + dir[k][1];
+
+            if(nr >= 0 && nr < n && nc >= 0 && nc < m){
+                let newEffort = Math.max(Math.abs(heights[nr][nc] - heights[r][c]), diff);
+                if(newEffort < dist[nr][nc]){
+                    dist[nr][nc] = newEffort;
+
+                    let newElement = [newEffort, nr, nc];
+                    // Do Binary Search to plane the shorter dist in the right for O(1) pop
+                    let low = 0, high = pq.length;
+                    while(low < high){
+                        let mid = (low + high) >> 1;
+                        if(pq[mid][0] < newEffort) high = mid;
+                        else low = mid + 1;
+                    }
+
+                    pq.splice(low, 0, newElement);
+                }
             }
         }
     }
-    return effort[rows - 1][columns - 1];
+    return 0;
 };
-
-class MyMinHeap {
-    constructor() {
-        this.heap = [];
-    }
-    size() {
-        return this.heap.length;
-    }
-    peek() {
-        return this.heap[0];
-    }
-    insert([row, column, effort]) {
-        this.heap.push([row, column, effort]);
-        let index = this.heap.length - 1;
-        let parentIndex = Math.floor((index - 1) / 2);
-        while (index > 0) {
-            if (this.heap[parentIndex][2] <= this.heap[index][2]) {
-                break;
-            }
-
-            let temp = this.heap[parentIndex];
-            this.heap[parentIndex] = this.heap[index];
-            this.heap[index] = temp;
-            index = parentIndex;
-        }
-
-    }
-    removeMin() {
-        if (this.heap.length === 0) return null;
-        if (this.heap.length === 1) return this.heap.pop();
-
-        let min = this.heap[0];
-        this.heap[0] = this.heap.pop();
-        let index = 0;
-        while (true) {
-            let minIndex = index;
-            let leftChildIndex = (index * 2) + 1;
-            let rightChildIndex = (index * 2) + 2;
-            if (leftChildIndex < this.heap.length && this.heap[leftChildIndex][2] < this.heap[minIndex][2]) {
-                minIndex = leftChildIndex;
-            }
-
-            if (rightChildIndex < this.heap.length && this.heap[rightChildIndex][2] < this.heap[minIndex][2]) {
-                minIndex = rightChildIndex;
-            }
-            if (minIndex == index) break;
-
-            let temp = this.heap[minIndex];
-            this.heap[minIndex] = this.heap[index];
-            this.heap[index] = temp;
-            index = minIndex;
-        }
-        return min;
-    }
-}
